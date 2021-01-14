b9594bace4c9a0b13326ce53dcd82dcd1728a895
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-14 11:26:28 +0700

    Update types

--- types/src/sentry-loglevel.d.ts
+++ types/src/sentry-loglevel.d.ts
@@ -1,6 +1,7 @@
-import { BrowserOptions } from "@sentry/browser";
+import { BrowserOptions, Severity } from "@sentry/browser";
 import { Logger } from "loglevel";
 export default function installSentry(logger: Logger, opts: BrowserOptions): {
+    log: (level: Severity, ...msgs: unknown[]) => void;
     trace: (...msgs: unknown[]) => void;
     error: (err: Error, ...msgs: unknown[]) => void;
 };
