b23512c5c95883e6b3493d75744cba80ae4173e7
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-02 17:06:49 +0700

    Update exports (#12)
    * Update exports
    
    * Update types

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { createLogger } from "./src/create-logger";
 export { default } from "./src/loglevel-sentry";
-export { redactData } from "./src/redact-data";
+export * from "./src/redact-data";
