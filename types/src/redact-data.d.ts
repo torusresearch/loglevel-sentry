67db09f90e76c4dd85931c6f785168f3b11ccd9c
chaitanyapotti <chaitanya.potti@gmail.com>
2021-06-02 17:26:01 +0800

    add types

--- types/src/redact-data.d.ts
+++ types/src/redact-data.d.ts
@@ -1,1 +1,6 @@
+export declare function redactEventData<T = unknown>(data: T, keyPattern?: RegExp): T;
+export declare function redactBreadcrumbData<T = unknown>(data: T): T;
+/**
+ * @deprecated Use `redactEventData` instead
+ */
 export declare function redactData<T = unknown>(data: T, keyPattern?: RegExp): T;
