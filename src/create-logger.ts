69a8f776427526335670211bbbd428f65a63101b
Tai Nguyen TT <tai.nguyen@tor.us>
2025-04-03 23:41:08 +0700

    deprecate createLogger method as it may cause nested error message

--- src/create-logger.ts
+++ src/create-logger.ts
@@ -2,6 +2,12 @@ import loglevel, { Logger } from "loglevel";
 
 import { LoglevelSentry as SentryPlugin, Sentry } from "./loglevel-sentry";
 
+/**
+ * TODO: remove in the next major version (v10)
+ * @deprecated Use `loglevel.getLogger` instead
+ * We should not use this function as it'll install the plugin to the logger which we already do it when setting up the Sentry
+ * When we install multiple times, it'll format the error message in a nested way
+ */
 export function createLogger(name: string, sentry: Sentry): Logger {
   const logger = loglevel.getLogger(name);
   logger.enableAll();
